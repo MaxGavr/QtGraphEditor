132f3c4c260cedd1f20b216e52f6f887cf80ed1e
Max <max.gavr@tut.by>
2017-03-02 00:26:55 +0300

    Menu bar and toolbar added, but menu actions are still useless.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,168 @@
 #include "mainwindow.h"
+#include <QtGui>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QAction>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QApplication>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    drawingArea = new QWidget;
+    setCentralWidget(drawingArea);
+
+    createMenuActions();
+    createMenus();
+    createToolBars();
+
+    //setCurrentFile("");
 }
 
 MainWindow::~MainWindow()
 {
 
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void MainWindow::createMenuActions()
+{
+    newFileAction = new QAction(tr("&New file"), this);
+    newFileAction->setShortcut(QKeySequence::New);
+    newFileAction->setStatusTip(tr("Create new file"));
+    connect(newFileAction, SIGNAL(triggered()), this, SLOT(newFile()));
+
+    openFileAction = new QAction(tr("&Open file"), this);
+    openFileAction->setShortcut(QKeySequence::Open);
+    openFileAction->setStatusTip(tr("Open existing file"));
+    connect(openFileAction, SIGNAL(triggered()), this, SLOT(open()));
+
+    saveFileAction = new QAction(tr("&Save file"), this);
+    saveFileAction->setShortcut(QKeySequence::Save);
+    saveFileAction->setStatusTip(tr("Save current file"));
+    connect(saveFileAction, SIGNAL(triggered()), this, SLOT(save()));
+
+    saveAsFileAction = new QAction(tr("&Save file as"), this);
+    saveAsFileAction->setShortcut(QKeySequence::SaveAs);
+    saveAsFileAction->setStatusTip(tr("Save current file in a certain way"));
+    connect(saveAsFileAction, SIGNAL(triggered()), this, SLOT(saveAs()));
+
+    exitAction = new QAction(tr("E&xit"), this);
+    exitAction->setShortcut(tr("Ctrl+Q"));
+    exitAction->setStatusTip(tr("Exit the application"));
+    connect(exitAction, SIGNAL(triggered()), this, SLOT(close()));
+
+    aboutQtAction = new QAction(tr("About &Qt"), this);
+    aboutQtAction->setStatusTip(tr("Show information about Qt library"));
+    connect(aboutQtAction, SIGNAL(triggered()), qApp, SLOT(aboutQt()));
+}
+
+void MainWindow::createMenus()
+{
+    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu->addAction(newFileAction);
+    fileMenu->addAction(openFileAction);
+    fileMenu->addAction(saveFileAction);
+    fileMenu->addAction(saveAsFileAction);
+
+    fileMenu->addSeparator();
+    fileMenu->addAction(exitAction);
+
+    editMenu = menuBar()->addMenu(tr("&Edit"));
+
+    menuBar()->addSeparator();
+
+    helpMenu = menuBar()->addMenu(tr("&Help"));
+    helpMenu->addAction(aboutQtAction);
+}
+
+void MainWindow::createToolBars()
+{
+    fileToolBar = addToolBar(tr("&File"));
+    fileToolBar->addAction(newFileAction);
+    fileToolBar->addAction(openFileAction);
+    fileToolBar->addAction(saveFileAction);
+
+    editToolBar = addToolBar(tr("&Edit"));
+}
+
+void MainWindow::newFile()
+{
+    if (saveConfirmation())
+    {
+        // TO DO: creating new graph file
+    }
+}
+
+bool MainWindow::saveConfirmation()
+{
+    // TO DO: ignore confirmation if graph has not been modified
+    int respond = QMessageBox::warning(this, tr("Saving"),
+                                       tr("Graph has been modified.\n"
+                                          "Do you want to save changes?"),
+                                       QMessageBox::Yes | QMessageBox::No |
+                                       QMessageBox::Cancel);
+    if (respond == QMessageBox::Yes)
+        return save();
+    else if (respond == QMessageBox::Cancel)
+        return false;
+    return true;
+}
+
+void MainWindow::open()
+{
+    if (saveConfirmation()) // save current file before opening another one
+    {
+        QString fileName = QFileDialog::getOpenFileName(this,
+                                                        tr("Open graph"), ".",
+                                                        tr("Graph files (*.gph)"));
+        if (!fileName.isEmpty())
+            loadFile(fileName);
+    }
+}
+
+bool MainWindow::loadFile(const QString &fileName)
+{
+    if (saveConfirmation())
+    {
+        // TO DO: loading existing graph file
+    }
+    return true;
+}
+
+bool MainWindow::save()
+{
+    return true;
+}
+
+bool MainWindow::saveFile(const QString &fileName)
+{
+    return true;
+}
+
+bool MainWindow::saveAs()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,
+                                                    tr("Save graph as"), ".",
+                                                    tr("Graph files (*.gph)"));
+    if (fileName.isEmpty())
+        return false;
+
+    return saveFile(fileName);
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    if (saveConfirmation())
+    {
+        // TO DO: saving settings
+        event->accept();
+    }
+    else
+    {
+        event->ignore();
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,8 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
+#pragma once
 #include <QMainWindow>
+
+class QAction;
+class QWidget;
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +11,53 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
-};
 
-#endif // MAINWINDOW_H
+protected:
+    void closeEvent(QCloseEvent *event);
+
+private slots:
+    void newFile();
+    void open();
+    bool save();
+    bool saveAs();
+    //void openRecentFile();
+
+private:
+    // initializing functions
+    void createMenuActions();
+    void createMenus();
+    void createToolBars();
+
+    bool saveConfirmation();
+
+    bool saveFile(const QString& fileName);
+    bool loadFile(const QString& fileName);
+
+    void setCurrentFile(const QString& fileName);
+    //void updateRecentFilesOpenActions();
+
+    /*QStringList recentFiles;
+    QString currentFile;
+    const int MaxRecentFiles = 5;
+    QAction* recentFilesOpenActions[MaxRecentFiles];
+    QAction* separatorAction;*/
+
+    // menus and toolbars
+    QMenu* fileMenu;
+    QMenu* editMenu;
+    QMenu* helpMenu;
+    QToolBar* fileToolBar;
+    QToolBar* editToolBar;
+
+    // menu and toolbar actions
+    QAction* newFileAction;
+    QAction* openFileAction;
+    QAction* saveFileAction;
+    QAction* saveAsFileAction;
+    QAction* exitAction;
+    QAction* aboutQtAction;
+
+    // central widget, where graphs will be drawn
+    // for now, it's just blank QWidget
+    QWidget* drawingArea;
+};
